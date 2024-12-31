6310cf031ef4810806e04c6fb7e48ad59b199de7
ASUS Desktop <1033718596@qq.com>
2024-12-31 00:15:29 -0800

    update scripts

--- uscripts/run.sh
+++ uscripts/run.sh
@@ -82,3 +82,23 @@ accelerate launch --num_processes=1 \
 --log_samples \
 --log_samples_suffix vlr \
 --output_path ./logs/
+
+accelerate launch --num_processes=1 \
+-m lmms_eval \
+--model vlr \
+--model_args pretrained=lmms-lab/llava-onevision-qwen2-0.5b-si,conv_template=qwen_1_5,model_name=llava_qwen \
+--tasks vqav2_val \
+--batch_size 1 \
+--log_samples \
+--log_samples_suffix vlr \
+--output_path ./logs/
+
+accelerate launch --num_processes=4 \
+-m lmms_eval \
+--model llava_onevision \
+--model_args pretrained=lmms-lab/llava-onevision-qwen2-0.5b-si,conv_template=qwen_1_5,model_name=llava_qwen \
+--tasks vqav2_val \
+--batch_size 1 \
+--log_samples \
+--log_samples_suffix llava_onevision \
+--output_path ./logs/
