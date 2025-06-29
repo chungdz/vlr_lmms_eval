88178117102b14be7b23a4d8dd554c7845f01abb
ASUS Desktop <1033718596@qq.com>
2025-06-29 12:33:23 -0700

    update model

--- uscripts/run.sh
+++ uscripts/run.sh
@@ -83,7 +83,7 @@ accelerate launch --num_processes=1 \
 
 accelerate launch --num_processes=1 \
 -m lmms_eval \
---model vlr \
+--model llava_onevision \
 --model_args pretrained=lmms-lab/llava-onevision-qwen2-0.5b-si,conv_template=qwen_1_5,model_name=llava_qwen \
 --tasks vqav2_val_lite \
 --batch_size 1 \
